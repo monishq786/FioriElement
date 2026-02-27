479ec5e8ff5275d819a571b985f57120860dfb65
Moh Monish <mohdmonish8882@gmail.com>
2026-02-27 09:47:57 +0000

    changes for table

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,7 +5,17 @@ sap.ui.define([
 
     return Controller.extend("nwproductsdata.controller.View1", {
         onInit() {
-          
+          let oModelProd = this.getOwnerComponent().getModel();
+          oModelProd.read('/Products',{
+            success : (oData) =>{
+             let oModel = new sap.ui.model.json.JSONModel();
+             oModel.setData(oData.results);
+             this.getView().setModel(oModel,'ProdModel');
+            },
+            error : (oError) =>{
+
+            }
+          })
         }
     });
 });
