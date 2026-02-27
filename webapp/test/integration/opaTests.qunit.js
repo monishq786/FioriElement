a89aa81b0da9cdc57a68c5d60e3d38614448f795
Moh Monish <mohdmonish8882@gmail.com>
2026-02-27 07:58:18 +0000

    changes for NW

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["nwproductsdata/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"nwproductsdata/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
